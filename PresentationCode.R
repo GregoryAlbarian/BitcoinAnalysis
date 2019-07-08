33e926bba6e77f91c3d4da6c7fcb19769f9b4d7a
GregoryAlbarian <44635174+GregoryAlbarian@users.noreply.github.com>
2019-07-07 21:46:11 -0700

    Presentation on my computer

---
+++ PresentationCode.R
@@ -0,0 +1,56 @@
+install.packages("ggplot2")#, "reshape2", "plyr", "languageR", "lme4", "psych")
+
+library(ggplot2)
+library(plyr)
+
+bitcoinData<-read.csv(file.choose())
+
+head(bitcoinData[3:6])
+tail(bitcoinData[3:6])
+
+#bitcoinData
+
+
+summary(bitcoinData[3:6])
+
+bitcoinData$range<-bitcoinData$high-bitcoinData$low
+#bitcoinData$range
+summary(bitcoinData$range)
+
+bitcoinData$priceChange<-bitcoinData$open-bitcoinData$close
+summary(bitcoinData$priceChange)
+
+bitcoinData$counter<-seq(1:nrow(bitcoinData))
+#bitcoinData$counter
+
+#gg<-ggplot(bitcoinData, aes(x=counter, y=close))
+#gg<-gg+geom_point(colour="blue")
+#gg<-gg + labs(x = "New x label")
+
+
+#graph 1
+gg<-ggplot(bitcoinData, aes(x=counter, y=priceChange)) + geom_point(colour="blue")
+gg + labs(x= "days")
+
+#graph 2
+gg2<-ggplot(bitcoinData, aes(x=close, y=range)) + geom_point(colour="red")
+gg2 + labs(x= "days")
+
+#graph 3
+gg3<-ggplot(data=bitcoinData, aes(bitcoinData$range)) + geom_histogram(colour="red", fill="yellow")
+gg3+ labs(x= "range")
+
+#graph 4
+gg4<-ggplot(data=bitcoinData, aes(bitcoinData$priceChange)) + geom_histogram(colour="red", fill="yellow")
+gg4+ labs(x= "price change")
+
+#need to do these
+#comparing with the price of precious metals because it has non-inflationary value
+
+
+gg5<-ggplot(bitcoinData, aes(x=counter, y=priceChange)) + geom_point(colour="blue")
+gg5 + labs(x= "days")
+
+
+gg6<-ggplot(bitcoinData, aes(x=close, y=range)) + geom_point(colour="green")
+gg6 + labs(x= "days")
